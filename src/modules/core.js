d06212d74a7bc3adccea23e5cce5c64db7cd148d
NA brain <michal2002.60@gmail.com>
2024-01-13 14:52:22 +0100

    add core website with navbar, main section and footer

--- src/modules/core.js
+++ src/modules/core.js
@@ -1,26 +1,62 @@
 function createNav() {
     const selectContent = document.getElementById('content');
 
-    //create and add #nav
+    //create and append #nav
     const selectNav = document.createElement('div');
     selectNav.id='nav';
     selectContent.appendChild(selectNav);
-    
-    //create and add #main
-    const selectMain = document.createElement('main');
+
+    //create and append Nav elements
+    createNavItem('home', 'Home', selectNav);
+    createNavItem('menu', 'Menu', selectNav);
+    createNavItem('find-us', 'Find Us', selectNav);
+}
+
+function createNavItem(id, text, selectNav) {
+    const navItemComponent = document.createElement('div')
+    const selectImg = document.createElement('div');
+    const selectNavItem = document.createElement('div');
+    selectImg.id = `${id}-img`;
+    selectNavItem.id= id;
+    selectNavItem.innerText = text;
+    navItemComponent.append(selectImg, selectNavItem);
+    selectNav.appendChild(navItemComponent);
+}
+
+function createMain() {
+    const selectContent = document.getElementById('content');
+
+    //create and append #main
+    const selectMain = document.createElement('div');
     selectMain.id='main';
     selectContent.appendChild(selectMain);
 
-    //create and add #main-text
-    const selectMainText = document.createElement('main-text');
+    //create and append #main-text
+    const selectMainText = document.createElement('div');
     selectMainText.id='main-text';
     selectMain.appendChild(selectMainText);
+}
 
-    //create and add #footer
-    const selectFooter = document.createElement('footer');
+function createFooter() {
+    const selectContent = document.getElementById('content');
+
+    //create and append #footer
+    const selectFooter = document.createElement('div');
     selectFooter.id='footer';
     selectContent.appendChild(selectFooter);
+    createFooterItem(selectFooter);
 }
+
+function createFooterItem(selectFooter) {
+    const selectFooterItem = document.createElement('div');
+    selectFooterItem.id='footer-item';
+    selectFooterItem.innerText = 'All rights reserved.';
+    selectFooter.appendChild(selectFooterItem);
+}
+
+
 export default function initializeWebsite() {
     createNav();
+    createMain();
+    createFooter();      
 }

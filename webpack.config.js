cd55bd8cc9c2c72092d4335ba8d707a52d5062cd
NA brain <michal2002.60@gmail.com>
2024-01-14 18:49:10 +0100

    test

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,25 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: {
     index: './src/index.js',
   },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Coffee Website',
+    }),
+  ],
+
   devtool: 'inline-source-map',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
+
   module: {
     rules: [
       {
